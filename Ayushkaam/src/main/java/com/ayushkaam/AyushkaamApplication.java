3b89362fcb32127c5603973c7c61212fecf6df88
rohan209547mourya <rohan.209547@gmail.com>
2022-11-09 21:20:41 +0530

    Project created

---
+++ Ayushkaam/src/main/java/com/ayushkaam/AyushkaamApplication.java
@@ -0,0 +1,13 @@
+package com.ayushkaam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyushkaamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyushkaamApplication.class, args);
+	}
+
+}
---
+++ Ayushkaam/src/test/java/com/ayushkaam/AyushkaamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayushkaam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyushkaamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
