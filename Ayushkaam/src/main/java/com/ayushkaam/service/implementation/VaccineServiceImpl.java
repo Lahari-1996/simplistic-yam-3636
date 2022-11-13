3933f02c1507fc341e5d80b85543498f4d0c20ea
Himanshu Sharma <himanshu241100@gmail.com>
2022-11-13 18:57:17 +0530

    vaccinesrviceimpl

--- Ayushkaam/src/main/java/com/ayushkaam/service/implementation/VaccineServiceImpl.java
+++ Ayushkaam/src/main/java/com/ayushkaam/service/implementation/VaccineServiceImpl.java
@@ -1,10 +1,15 @@
 package com.ayushkaam.service.implementation;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ayushkaam.exception.VaccineNotFoundException;
+import com.ayushkaam.model.Appointment;
+import com.ayushkaam.model.CurrentAdminSession;
+import com.ayushkaam.model.MemberSession;
 import com.ayushkaam.model.Vaccine;
 import com.ayushkaam.repository.AppointmentDao;
 import com.ayushkaam.repository.MemberSessionDao;
@@ -25,38 +30,115 @@ public class VaccineServiceImpl implements VaccineService{
 
 	@Override
 	public List<Vaccine> allVaccine(String key) {
-		// TODO Auto-generated method stub
-		return null;
+	
+		Optional<Appointment> optCurrAdmin = appointmentDAO.findByMembers(key);
+		
+		Optional<MemberSession> optMembSession = memberSessionDAO.findByToken(key);
+		
+		if(!optCurrAdmin.isPresent()&&!optMembSession.isPresent()) {
+			
+			throw new RuntimeException("Unauthorised access")
+		}
+		
+		List<Vaccine> list = dao.findAll();
+
+		if (list.size() > 0)
+			return list;
+		else
+			throw new VaccineNotFoundException("No Vaccines Available");
 	}
 
 	@Override
 	public Vaccine getVaccineByName(String VaccineName, String key) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Optional<Appointment> optCurrAdmin = appointmentDAO.findByMembers(key);
+		
+		if(!optCurrAdmin.isPresent()) {
+			throw new RuntimeException("Unauthorised access");
+		}
+		
+       Optional<Vaccine> vaccine = dao.findByvaccinename(VaccineName);
+		
+		if(vaccine != null) return vaccine.get();
+		else throw new VaccineNotFoundException("Vaccine with name " + VaccineName + " in not available");
+		
+		
 	}
 
 	@Override
 	public Vaccine getVaccineById(Integer vaccineId, String key) {
-		// TODO Auto-generated method stub
-		return null;
+		
+      Optional<Appointment> optCurrAdmin = appointmentDAO.findByMembers(key);
+		
+		if(!optCurrAdmin.isPresent()) {
+			throw new RuntimeException("Unauthorised access");
+		}
+		Optional<Vaccine> opt = dao.findById(vaccineId);
+
+		if (opt.isPresent())
+			return opt.get();
+
+		else
+			throw new VaccineNotFoundException("Vaccine with Id " + vaccineId + " is not available");
+		
 	}
 
 	@Override
 	public Vaccine addVaccine(Vaccine vaccine, String key) {
-		// TODO Auto-generated method stub
-		return null;
+	
+		 Optional<Appointment> optCurrAdmin = appointmentDAO.findByMembers(key);
+			
+			if(!optCurrAdmin.isPresent()) {
+				throw new RuntimeException("Unauthorised access");
+			}
+			Optional<Vaccine> vacc = dao.findByvaccinename(vaccine.getVaccinename());
+			
+			if(vacc.get() == null) {
+
+
+	            return dao.save(vaccine);
+			}
+			throw new RuntimeException("Vaccine already exists!");	
+		
 	}
 
+	
 	@Override
 	public Vaccine updateVaccine(Vaccine vaccine, String key) {
-		// TODO Auto-generated method stub
-		return null;
+	
+		Optional<Appointment> optCurrAdmin = appointmentDAO.findByMembers(key);
+		
+		if(!optCurrAdmin.isPresent()) {
+			throw new RuntimeException("Unauthorised access");
+		}
+		
+		Optional<Vaccine> opt = dao.findById(vaccine.getVaccineId());
+
+		if (opt.isPresent()) {
+			return dao.save(vaccine);
+		} else
+			throw new VaccineNotFoundException("The vaccine you want to update does not exist!");
+		
 	}
 
+	
+	
 	@Override
 	public boolean deleteVaccine(Vaccine vaccine, String key) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		
+Optional<Appointment> optCurrAdmin = appointmentDAO.findByMembers(key);
+		
+		if(!optCurrAdmin.isPresent()) {
+			throw new RuntimeException("Unauthorised access");
+		}
+		Optional<Vaccine> opt = dao.findById(vaccine.getVaccineId());
+
+		if (opt.isPresent()) {
+			dao.delete(vaccine);
+			return true;
+		} else
+			throw new VaccineNotFoundException("The vaccine you want to delete does not exist!");
 	}
 	
 	
